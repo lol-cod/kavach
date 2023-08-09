44c4d07f92914a5907b51a20d9cb9d4560e7e72e
hiyasharma <95416551+hiyasharma@users.noreply.github.com>
2023-08-09 06:05:16 +0530

    Update parent_child_relation_ui.py

--- 8August 2023/parent_child_relation_ui.py
+++ 8August 2023/parent_child_relation_ui.py
@@ -2,6 +2,48 @@ import tkinter as tk
 from tkinter import ttk
 import psutil
 
+def show_warning_message():
+    popup = tk.Toplevel()
+    popup.title("Warning")
+    popup.configure(bg="red")
+
+    message = "Suspicious Activity Detected!"
+    label = tk.Label(popup, text=message, fg="black", bg="red")
+    label.pack(padx=80, pady=20)
+
+    warning_label = tk.Label(popup, text="⚠️", font=("Arial", 40), fg="black", bg="red")
+    warning_label.pack()
+
+    close_button = tk.Button(popup, text="OK", command=popup.destroy)
+    close_button.pack(pady=25)
+
+def find_and_display_process():
+    process_name = process_name_entry.get()
+    target_process = None
+
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'].lower() == process_name.lower():
+            target_process = process
+            break
+
+    if target_process is None:
+        result_label.config(text=f"Process not found: {process_name}", foreground="black")
+        show_warning_message()
+    else:
+        process_id = target_process.info['pid']
+        process_name = target_process.info['name']
+        process_path = get_process_path(process_id)
+        parent_process_id, parent_process_name, parent_process_path = get_parent_process_info(process_id)
+
+        result_label.config(text="Target Process Information:\n" +
+                                 f"Process Name: {process_name}\n" +
+                                 f"Process ID: {process_id}\n" +
+                                 f"Process Path: {process_path}\n" +
+                                 f"Parent Process ID: {parent_process_id}\n" +
+                                 f"Parent Process Name: {parent_process_name}\n" +
+                                 f"Parent Process Path: {parent_process_path}", foreground="green")
+        find_button.config(background="green")  # Make the button green when a process is found
+
 def get_parent_process_info(process_id):
     try:
         parent = psutil.Process(process_id)
@@ -15,31 +57,6 @@ def get_process_path(process_id):
         return process.exe()
     except (psutil.NoSuchProcess, psutil.AccessDenied):
         return "Access denied for this PID: " + str(process_id)
-
-def find_and_display_process():
-    process_name = process_name_entry.get()
-    target_process = None
-
-    for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'].lower() == process_name.lower():
-            target_process = process
-            break
-
-    if target_process is None:
-        result_label.config(text=f"Process not found: {process_name}")
-    else:
-        process_id = target_process.info['pid']
-        process_name = target_process.info['name']
-        process_path = get_process_path(process_id)
-        parent_process_id, parent_process_name, parent_process_path = get_parent_process_info(process_id)
-
-        result_label.config(text="Target Process Information:\n" +
-                                 f"Process Name: {process_name}\n" +
-                                 f"Process ID: {process_id}\n" +
-                                 f"Process Path: {process_path}\n" +
-                                 f"Parent Process ID: {parent_process_id}\n" +
-                                 f"Parent Process Name: {parent_process_name}\n" +
-                                 f"Parent Process Path: {parent_process_path}")
 
 # Create the main window
 root = tk.Tk()
